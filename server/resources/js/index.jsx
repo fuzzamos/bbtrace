ff4161af284351c199727b3ab982550935bf91d1
Fadhil Mandaga <firodj@gmail.com>
2017-07-11 02:44:46 +0700

    material ui

--- server/resources/js/index.jsx
+++ server/resources/js/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap 
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 // Components
-class App extends React.Component {
-  render () {
-    return <p>Ehm... Hello React!</p>;
-  }
-}
+const App = () => (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+  </MuiThemeProvider>
+);
 
 // Sytylesheets
 require('../../node_modules/roboto-fontface/css/roboto/sass/roboto-fontface-regular.scss');
 require('../scss/index.scss');
 
-render(<App/>, document.getElementById('app'));
+render(
+  <App />,
+  document.getElementById('app')
+);
