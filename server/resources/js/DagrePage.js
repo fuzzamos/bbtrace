e0880185bb627a3e0a8c241d21c9c23b939649ad
Fadhil Mandaga <firodj@gmail.com>
2017-10-20 17:24:36 +0700

    using draggable

--- server/resources/js/DagrePage.js
+++ server/resources/js/DagrePage.js
@@ -4,20 +4,13 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 import _ from 'lodash';
 import { Graph, Rect, Edge, NormalArrow } from './dagre';
+import DraggableCore from 'react-draggable';
 
 type Props = {};
-type State = {
-  offsetX: number,
-  offsetY: number
-}
+type State = {}
 
 class DagrePage extends Component<Props, State> {
   lastMousePos = {x: 0, y: 0, dragging: false}
-
-  state = {
-    offsetX: 0,
-    offsetY: 0
-  }
 
   render() {
     const styles = {
@@ -35,15 +28,11 @@ class DagrePage extends Component<Props, State> {
 
     return (
       <div style={styles.paper} id="mainPaper">
-        <svg width="100%" height="100%"
-          onMouseDown={this.handlePanning}
-          onMouseUp={this.handlePanning}
-          onMouseMove={this.handlePanning}
-        >
-          <defs>
-            <NormalArrow id="markerArrow" />
-          </defs>
-          <g transform={`translate(${this.state.offsetX},${this.state.offsetY})`}>
+        <DraggableCore>
+          <svg width="100%" height="100%">
+            <defs>
+              <NormalArrow id="markerArrow" />
+            </defs>
             <Graph>
               <Rect key="foo" style={{ fill: 'red' }} onClick={(e) => console.log(e)}>
                 <text fontSize="10" fontFamily="Verdana">
@@ -63,40 +52,15 @@ class DagrePage extends Component<Props, State> {
               </Rect>
               <Edge markerEnd="url(#markerArrow)" source="foo" target="bar">
                 <text>
-        { this.state.offsetX }
+                  Yes
                 </text>
               </Edge>
               <Edge markerEnd="url(#markerArrow)" source="foo" target="baz" />
             </Graph>
-          </g>
-        </svg>
+          </svg>
+        </DraggableCore>
       </div>
     )
-  }
-
-  handlePanning = (e: SyntheticMouseEvent<HTMLElement>) => {
-    if (e.type == 'mousedown') {
-      if (e.target.tagName == 'svg') {
-        this.lastMousePos = {x: e.clientX, y: e.clientY, dragging: true};
-        e.preventDefault();
-      }
-    } else if (e.type == 'mousemove') {
-      if (this.lastMousePos.dragging) {
-        const panX = e.clientX - this.lastMousePos.x;
-        const panY = e.clientY - this.lastMousePos.y;
-        this.setState(prevState => ({
-            offsetX: prevState.offsetX + panX,
-            offsetY: prevState.offsetY + panY
-        }));
-        this.lastMousePos = {x: e.clientX, y: e.clientY, dragging: true};
-        e.preventDefault();
-      }
-    } else if (e.type == 'mouseup') {
-      if (this.lastMousePos.dragging) {
-        this.lastMousePos = {x: e.clientX, y: e.clientY, dragging: false};
-        e.preventDefault();
-      }
-    }
   }
 }
 
